4ded8b13ee1212fc15234bc96176bdf3713541a4
NissimBansal <aryan.nissim@gmail.com>
2026-09-17 18:58:23 +0530

    Updated imm_gen.v

--- imm_gen.v
+++ imm_gen.v
@@ -7,15 +7,18 @@ wire signed [31:0] instr_signed = instr;
 always @ (instr) begin
     case (instr[6:0])
 
-        7'b0010011 : case (instr[14:12]) // I-Type(arithemtic)
+        7'b0010011 : begin case (instr[14:12]) // I-Type(arithemtic)
 
-                        defualt : immediate = instr_signed >>> 19; // for logical/add
+                        default : immediate = instr_signed >>> 19; // for logical/add
 
                         3'b011 : immediate = instr >> 19; // for sltiu
 
                         3'b101 : immediate = {{27{1'b0}},instr[24:20]}; // for srli/srai
 
                     endcase
+                    end
+        
+        7'b0110011 : immediate = 32'b0; // R-Type
 
         default : immediate = 32'b0; 
     endcase

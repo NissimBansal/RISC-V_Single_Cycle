1a85d8c1e9ffed534b523bd339a7bc622d7ab8b4
NissimBansal <aryan.nissim@gmail.com>
2026-09-20 16:48:14 +0530

    Updated Reg File

--- reg_file.v
+++ reg_file.v
@@ -7,22 +7,16 @@ module reg_file (   input [31:0] instr,
 
 reg [31:0] register [31:0]; // 32-bit register file with ach register being 32 bits wide
 
-reg [4:0] addrA; // rs1
-reg [4:0] addrB; // rs2
-reg [4:0] addrD; // rd
+wire [4:0] addrA = instr[19:15]; // capture rs1
+wire [4:0] addrB = instr[24:20]; // capture rs2
+wire [4:0] addrD = instr[11:7]; // capture rd
 
-always @ (*) register[0] = 32'b0;
+always @ (instr or clk or we_reg) register[0] = 32'b0;
 
 always @ (posedge clk) begin
-
-addrA <= instr [19:15];
-addrB <= instr [24:20];
-addrD <= instr [11:7];
-
-if ((we_reg) && (addrD != 5'b0)) begin // to prevent writing into x0!
-    register[addrD] <= ALU_op;
-end
-
+    if ((we_reg) && (addrD != 5'b0)) begin // to prevent writing into x0 register
+        register[addrD] <= ALU_op;
+    end
 end
 
 assign DataA = (addrA == 5'b0) ? 32'b0 : register[addrA];

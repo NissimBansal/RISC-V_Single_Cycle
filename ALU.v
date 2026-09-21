d02e5fccafd3b1f1692b3da22dbc87ba1ff28529
NissimBansal <aryan.nissim@gmail.com>
2026-09-22 00:06:49 +0530

    Updated ALU

--- ALU.v
+++ ALU.v
@@ -1,11 +1,22 @@
-module ALU (input [31:0] DataA, DataB,
-            input ALUSel,
-            output ALU_output)
+module ALU (input signed [31:0] DataA, DataB,
+            input [3:0] ALUSel,
+            output reg [31:0] ALU_output)
 ;
 
 always @ (*) begin
-    if (!ALUSel) ALU_output = DataA + DataB;
-    else ALU_output = DataA - DataB;
+    case (ALUSel) 
+        4'b0000 : ALU_output = DataA + DataB; // add
+        4'b1000 : ALU_output = DataA - DataB; // sub
+        4'b0001 : ALU_output = DataA << DataB; // sll
+        4'b0010 : ALU_output = (DataA < DataB) // slt
+        4'b0011 : ALU_output = ($unsigned(DataA) < $unsigned(DataB)); // sltu
+        4'b0100 : ALU_output = DataA ^ DataB; // xor
+        4'b0101 : ALU_output = DataA >> DataB; // srl
+        4'b1101 : ALU_output = DataA >>> DataB; // sra
+        4'b0110 : ALU_output = DataA | DataB; // or
+        4'b0111 : ALU_output = DataA & DataB; // and
+        default : ALU_output = 32'b0; 
+    endcase
 end
 
 endmodule
